29face0385798f5a6a14f7aa448c71541208e261
sellitus <sellitus@gmail.com>
2021-11-01 00:09:47 -0500

    Tweaks

--- genetic-strategy-builder/install_pycron.sh
+++ genetic-strategy-builder/install_pycron.sh
@@ -1,4 +1,16 @@
 
+# Make sure the script is run as root
+if [[ $(/usr/bin/id -u) -ne 0 ]]; then
+    echo "ERROR: Script was not run under the root user. Use sudo"
+    exit 1
+fi
+
+if [ "$#" -ne 1 ]
+then
+  echo "ERROR: The machine's ID must be passed at the first argument"
+  echo "Usage: sudo bash install_pycron.sh 2"
+  exit 1
+fi
 
 echo "
 [Unit]
@@ -7,7 +19,7 @@ After=multi-user.target
 [Service]
 Type=simple
 Restart=always
-ExecStart=/home/sellitus/anaconda3/envs/stock-toolkit/bin/python /home/sellitus/PythonProjects/stock-toolkit/genetic-strategy-builder/pycron.py
+ExecStart=/home/sellitus/anaconda3/envs/stock-toolkit/bin/python /home/sellitus/PythonProjects/stock-toolkit/genetic-strategy-builder/pycron.py --id $1
 [Install]
 WantedBy=multi-user.target
 " > /etc/systemd/system/pycron.service
--- genetic-strategy-builder/pycron.py
+++ genetic-strategy-builder/pycron.py
@@ -1,11 +1,19 @@
-# Sets which system this, which changes the tickers that are run on it
-SYSTEM_ID = 1
 
-
+import argparse
 import schedule
 import time
 
 import subprocess
+
+
+parser = argparse.ArgumentParser(description='Python based cron alternative...for being able to send emails.')
+parser.add_argument('--id', dest="SYSTEM_ID", required=True, type=int,
+                    help="ID of the machine for running tests. Chooses the suite of tests to run so there is no "
+                         "overlap. This arg is used in the /etc/systemd/system/pycron.service file"
+                         " - Example [ --id 2 ]")
+args = parser.parse_args()
+
+SYSTEM_ID = args.SYSTEM_ID
 
 
 PYTHON_EXE = '/home/sellitus/anaconda3/envs/stock-toolkit/bin/python'
@@ -20,7 +28,7 @@ def run_on_ticker(ticker):
 
 
 def start_system_run(system_id):
-    crypto_morning_A = '9:30'
+    crypto_morning_A = '09:30'
     crypto_morning_B = '10:30'
 
     stock_candle_close_A = '12:30'
