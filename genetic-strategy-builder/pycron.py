b67d100dd7e1dbe99d1f823aa5f5300b59882b6d
sellitus <sellitus@gmail.com>
2021-10-31 22:54:45 -0500

    Refined scheduled pycron jobs

--- genetic-strategy-builder/pycron.py
+++ genetic-strategy-builder/pycron.py
@@ -11,7 +11,7 @@ import subprocess
 PYTHON_EXE = '/home/sellitus/anaconda3/envs/stock-toolkit/bin/python'
 GEN_ALGO_EXE = '/home/sellitus/PythonProjects/stock-toolkit/genetic-strategy-builder/gen_algo.py'
 
-NOTIFY_VAL = 1500
+NOTIFY_VAL = 1200
 
 
 def run_on_ticker(ticker):
@@ -20,29 +20,41 @@ def run_on_ticker(ticker):
 
 
 def start_system_run(system_id):
-    crypto_morning_A = '10:30'
+    crypto_morning_A = '9:30'
+    crypto_morning_B = '10:30'
     crypto_candle_close_A = '16:30'
-    stock_candle_close_A = '13:30'
 
+    stock_candle_close_A = '13:00'
+    stock_candle_close_B = '14:00'
+
+    # NOTE: Only add 3 of each symbol for each timeframe, otherwise it will become much slower
     if system_id == 1:
+        # Morning A
         schedule.every().day.at(crypto_morning_A).do(run_on_ticker, 'ADA-USD')
         schedule.every().day.at(crypto_morning_A).do(run_on_ticker, 'ETH-USD')
         schedule.every().day.at(crypto_morning_A).do(run_on_ticker, 'COTI-USD')
-        schedule.every().day.at(crypto_morning_A).do(run_on_ticker, 'ATOM1-USD')
+        # Morning B
+        schedule.every().day.at(crypto_morning_B).do(run_on_ticker, 'ATOM1-USD')
+        # Stock Candle Close A
         schedule.every().day.at(stock_candle_close_A).do(run_on_ticker, 'SPY')
         schedule.every().day.at(stock_candle_close_A).do(run_on_ticker, 'AMD')
         schedule.every().day.at(stock_candle_close_A).do(run_on_ticker, 'NVDA')
-        schedule.every().day.at(stock_candle_close_A).do(run_on_ticker, 'TSLA')
+        # Stock Candle Close B
+        schedule.every().day.at(stock_candle_close_B).do(run_on_ticker, 'TSLA')
 
     if system_id == 2:
+        # Morning A
         schedule.every().day.at(crypto_morning_A).do(run_on_ticker, 'DOT1-USD')
         schedule.every().day.at(crypto_morning_A).do(run_on_ticker, 'LINK-USD')
         schedule.every().day.at(crypto_morning_A).do(run_on_ticker, 'DOGE-USD')
-        schedule.every().day.at(crypto_morning_A).do(run_on_ticker, 'SHIB-USD')
+        # Morning B
+        schedule.every().day.at(crypto_morning_B).do(run_on_ticker, 'SHIB-USD')
+        # Stock Candle Close A
         schedule.every().day.at(stock_candle_close_A).do(run_on_ticker, 'MSFT')
         schedule.every().day.at(stock_candle_close_A).do(run_on_ticker, 'GOOGL')
         schedule.every().day.at(stock_candle_close_A).do(run_on_ticker, 'INTC')
-        schedule.every().day.at(stock_candle_close_A).do(run_on_ticker, 'AAPL')
+        # Stock Candle Close B
+        schedule.every().day.at(stock_candle_close_B).do(run_on_ticker, 'AAPL')
 
 
 while True:
